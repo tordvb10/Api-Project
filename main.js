670695d48c96b82e6892c6a68200f965e0abb5c8
tordvb10 <tordvb10.kodehode@gmail.com>
2024-02-22 10:47:45 +0100

    trenger hjelp med apikeys.

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-import * as helps from "./scripts/createbutton.js";
+import * as createbutton from "./scripts/createbutton.js";
 console.log("hello world");
-helps.sayHi();
+createbutton.sayHi();
+console.log(createbutton.button);
+
+const APIkey = "3af8b89d0b868bd0d3a19e92c6ae82bd";
+const bibelurl = `https://scripture.api.bible/admin/applications/api-key=${APIkey}`;
+
+const bibelFetch = async () => {
+  const response = await fetch(bibelurl);
+  const data = await response.json();
+  console.log(data);
+};
+
+bibelFetch();
--- scripts/createbutton.js
+++ scripts/createbutton.js
@@ -1,3 +1,5 @@
 export function sayHi() {
   console.log("Hi");
 }
+
+export const button = document.createElement("button");
