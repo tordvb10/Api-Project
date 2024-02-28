61a78ff0a2cce550d58cdf8e712b54c473113ff1
tordvb10 <tordvb10.kodehode@gmail.com>
2024-02-28 11:32:13 +0100

    klart å hente ut alle vers.

--- Bibelfetch.js
+++ Bibelfetch.js
@@ -15,18 +15,20 @@ const option = {
 const errorArray = []
 const scripturesFetch = async (input = "") => {
     try{
-        const response = await fetch(url+input, option)
-        const fetchedBibles = await response.json()
-        console.log(fetchedBibles)
+        let path = url+input
+        console.log(path)
+        const response = await fetch(path, option)
+        const fetched = await response.json()
         const norBible = []
-        fetchedBibles.data.forEach((element) => {
-            if(element.language.id === "nob"){
-                norBible.push(element)
-                return
-            }
-        })
-        console.log(norBible[0].id)
-        return norBible[0].id
+        if (input === ""){
+            fetched.data.forEach((element) => {
+                if(element.language.id === "nob"){
+                    norBible.push(element)
+                }
+            })
+        }
+        console.log(!!norBible.length)
+        return !!norBible.length ? norBible : fetched
     
     } catch (error){
         errorArray.push(error)
@@ -35,8 +37,22 @@ const scripturesFetch = async (input = "") => {
 }
 
 const fetchedBibles = await scripturesFetch()
+
 console.log(fetchedBibles)
 
-
-
-console.log(errorArray)
+const fetchedBooks = await scripturesFetch(`/${fetchedBibles[0].id}/books`)
+fs.writeFileSync("./verses.json","")
+const IDs = []
+fetchedBooks.data.forEach(async (Books) => {
+    const fetchChapters = await scripturesFetch(`/${fetchedBibles[0].id}/books/${Books.id}/chapters`)
+    fetchChapters.data.forEach(async (Chapter)=> {
+        if (!Chapter.id.includes("intro")){
+            const fetchedVerses = await scripturesFetch(`/${fetchedBibles[0].id}/chapters/${Chapter.id}/verses`)
+            fetchedVerses.data.forEach(async (vers)=> {
+               // console.log(vers.id)
+                IDs.push(vers.id)
+            })
+        }
+    })
+})
+console.log(IDs)
